034a1e29046ddf5dbd97576b257b927e58f02062
aoeminh <36764215+aoeminh@users.noreply.github.com>
2019-08-12 06:00:26 +0700

    create album fragment

---
+++ app/src/main/java/minh/quy/musicplayer/adapter/AlbumAdapter.kt
@@ -0,0 +1,67 @@
+package minh.quy.musicplayer.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_album_list.view.*
+import minh.quy.musicplayer.R
+import minh.quy.musicplayer.model.Album
+
+class AlbumAdapter(val context: Context) : RecyclerView.Adapter<AlbumAdapter.ViewHolder>() {
+    var albumList: MutableList<Album> = arrayListOf()
+    var currenImage = 1
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_album_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return albumList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.item.tv_album_name_item_album.text = albumList[position].albumName
+        holder.item.tv_number_track_item_album.text = albumList[position].songCount.toString()
+        when (currenImage) {
+            1 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_1)
+                currenImage++
+            }
+            2 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_2)
+                currenImage++
+            }
+            3 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_3)
+                currenImage++
+            }
+            4 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_4)
+                currenImage++
+            }
+            5 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_5)
+                currenImage++
+            }
+            6 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_6)
+                currenImage++
+            }
+            7 -> {
+                holder.item.img_item_album_list.setImageResource(R.drawable.album_art_7)
+                currenImage = 1
+            }
+
+
+        }
+    }
+
+    fun addAlbumList(albums: MutableList<Album>){
+        this.albumList.clear()
+        this.albumList.addAll(albums)
+        notifyDataSetChanged()
+    }
+    inner class ViewHolder(val item: View) : RecyclerView.ViewHolder(item)
+}
--- app/src/main/java/minh/quy/musicplayer/fragment/AlbumFragment.kt
+++ app/src/main/java/minh/quy/musicplayer/fragment/AlbumFragment.kt
@@ -1,16 +1,49 @@
 package minh.quy.musicplayer.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_album.*
 import minh.quy.musicplayer.R
+import minh.quy.musicplayer.activity.MainActivity
+import minh.quy.musicplayer.adapter.AlbumAdapter
 
 class AlbumFragment : Fragment() {
 
+    var alBumAdater: AlbumAdapter? = null
+    lateinit var alBumContext: Context
+    var mainActivity: MainActivity? = null
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        alBumContext = context!!
+        if(activity is MainActivity){
+            mainActivity = activity as MainActivity
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_album,container,false)
+        val view = inflater.inflate(R.layout.fragment_album, container, false)
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initRecyclerView()
+
+    }
+
+    fun initRecyclerView(){
+        rv_album_list.apply {
+            layoutManager = GridLayoutManager(alBumContext,2,RecyclerView.VERTICAL,false)
+            alBumAdater = AlbumAdapter(alBumContext)
+            alBumAdater?.addAlbumList(mainActivity!!.albumList)
+            adapter = alBumAdater
+        }
     }
 }
